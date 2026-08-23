1f067edc06810e81ee876111fb31d47f56530097
Naimul Islam <naimulrakib3@gmail.com>
2026-08-23 10:46:36 +0600

    fix: removed recursive backup folder from app router to fix vercel build

--- app/twistedai/app/utils/generateSlug.ts
+++
@@ -1,9 +0,0 @@
-export function generateSlug(length = 6) {
-  // Alphanumeric characters (avoiding confusing ones like l, I, 1, 0, O if you want)
-  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  let slug = '';
-  for (let i = 0; i < length; i++) {
-    slug += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-  return slug;
-}
